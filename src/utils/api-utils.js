0677946aeee453bd78aa6ab0eb50a02fde2b398a
morbiuslevakov <morbiuslevakov@gmail.com>
2024-06-07 19:38:37 +0300

    update

---
+++ src/utils/api-utils.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// const apiUrl = "https://api.deaslide.com/v1";
+const apiUrl = "http://192.168.1.102:9090/v1";
+
+export const apiConfig = {
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+export const fetchData = async (initData) => {
+
+    try {
+        axios.post(`${apiUrl}/auth`, { initData })
+            .then(response => {
+                const data = response.data;
+                if (data.success) {
+                    console.log('User authenticated successfully:', data.user);
+                } else {
+                    console.error('Authentication failed:', data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Error during authentication:', error);
+            });
+    } catch (error) {
+        throw error.response.data
+    }
+};
