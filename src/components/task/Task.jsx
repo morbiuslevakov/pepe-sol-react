db15739d6fe13a310ec127107ed8f7261ae25b44
morbiuslevakov <morbiuslevakov@gmail.com>
2024-06-17 10:27:52 +0300

    up

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Box } from '@mui/material';
+import {Typography, Box, Button} from '@mui/material';
 import { Arrow } from "../../static/Arrow";
 import { Done } from "../../static/Done";
 import { completeTask } from "../../utils/api-utils";
@@ -16,18 +16,20 @@ export const Task = ({id, index, title, reward, isCompleted}) => {
     }
 
     return (
-        <Box onClick={completeTask} style={{"display":"flex", "marginBottom":"15px"}}>
-            <Box style={{"width":"100%", "backgroundColor":"#0d0d0d", "display":"flex", "alignItems":"center", "paddingLeft":"10px", "paddingRight":"10px", "borderBottom":`1px solid ${color}`}}>
-                <Typography style={{"width":"10%", "fontFamily":"Manrope", "fontWeight":"100", "lineHeight":"1.25", "color":"#3b3b3b", "float":"left", "marginRight":"10px"}} fontSize={"3rem"}>
-                    {index}
-                </Typography>
-                <Typography style={{"fontFamily":"Manrope", "fontWeight":"500", "color":"#FFFFFF", "float":"left"}} fontSize={"0.75rem"}>
-                    <span>{title}</span><br/>
-                    <span>+{reward} NGP</span>
-                </Typography>
-                {isCompleted && <Done/>}
-                {!isCompleted && <Arrow/>}
+        <Button onClick={completeTask} style={{"border":"none", "boxShadow":"none"}}>
+            <Box style={{"display":"flex", "marginBottom":"15px"}}>
+                <Box style={{"width":"100%", "backgroundColor":"#0d0d0d", "display":"flex", "alignItems":"center", "paddingLeft":"10px", "paddingRight":"10px", "borderBottom":`1px solid ${color}`}}>
+                    <Typography style={{"width":"10%", "fontFamily":"Manrope", "fontWeight":"100", "lineHeight":"1.25", "color":"#3b3b3b", "float":"left", "marginRight":"10px"}} fontSize={"3rem"}>
+                        {index}
+                    </Typography>
+                    <Typography style={{"fontFamily":"Manrope", "fontWeight":"500", "color":"#FFFFFF", "float":"left"}} fontSize={"0.75rem"}>
+                        <span>{title}</span><br/>
+                        <span>+{reward} NGP</span>
+                    </Typography>
+                    {isCompleted && <Done/>}
+                    {!isCompleted && <Arrow/>}
+                </Box>
             </Box>
-        </Box>
+        </Button>
     );
 }
